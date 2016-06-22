45f0ca669fff1da93729023d8dea0050bafb9e66
J0na17anT98 <pilotj@outlook.com>
2016-06-22 14:50:46 -0400

    started missing persons app
    missing persons using microsofts facial recognition api

---
+++ MissingPersons/PersonCell.swift
@@ -0,0 +1,13 @@
+//
+//  PersonCell.swift
+//  MissingPersons
+//
+//  Created by Jonathan Tsistinas on 6/22/16.
+//  Copyright © 2016 Techinator. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+}
